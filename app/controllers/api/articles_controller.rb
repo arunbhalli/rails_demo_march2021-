c19dcdc1ba93c55d046f893b825ca76d85772605
arunbhalli <aruntomar8532014821@gmail.com.com>
2021-04-20 10:39:04 +0200

    work in progress

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -3,4 +3,13 @@ class Api::ArticlesController < ApplicationController
     articles = Article.all
     render json: {articles: articles}
   end
+  def show
+  end
+  def show 
+    article = Article.find(params[:id])
+    render json: {article: article }
+  end
+  def create 
+    binding.pry
+  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,6 +3,7 @@
 RSpec.describe Article, type: :model do
   describe 'DB table' do
     it{is_expected.to have_db_column(:title).of_type(:string)}
+    it {is_expected.to have_db_column(:body).of_type(:text)}
   end
 
   describe 'Factory' do
